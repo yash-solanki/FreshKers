061fe64e9344e2d7ce43a23bfd5aa61bd2e89830
Yash <yash.solanki@rapidops.com>
2019-09-25 19:13:57 +0530

    add path for connect mongo atlas

--- server/config/database.config.js
+++ server/config/database.config.js
@@ -1,3 +1,15 @@
 module.exports = {
     url: 'mongodb://localhost:27017/easy-notes'
 };
+
+
+// connect with mongo atlas
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://yash-solanki:yash@solanki@freshkers-mpnaz.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
